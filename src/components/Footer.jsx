de41a0b501c0114ef614abc58c7aed75b02ea4b2
moacir.fernandes <moacir.fernandes@tupiconsultoria.com.br>
2025-05-22 09:16:31 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import "./Footer.css";
+import { FaGithub, FaLinkedin, FaEnvelope, FaWhatsapp } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="footer">
+      <div className="footer-icons">
+        <a href="mailto:moacirsistemax@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FaEnvelope />
+        </a>
+        <a href="https://www.linkedin.com/in/moacir-fernandes-ba0a97a0/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Moa-fernandes" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://wa.me/5521973019585" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp />
+        </a>
+      </div>
       <p>&copy; 2025 Moacir Fernandes. Todos os direitos reservados.</p>
     </footer>
   );
